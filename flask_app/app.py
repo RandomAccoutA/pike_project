e1036ee3f442065d9293e26b1abc53f92b1cfb67
Fin Konnan <finkonnan@gmail.com>
2025-03-20 18:39:16 -0700

    Added gitignore

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,5 +1,12 @@
+import os
+
 from flask import Flask, render_template
-app = Flask(__name__)
+
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+)
 
 @app.route("/")
 def home():
