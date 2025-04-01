7e262adee57c31987481db067eda527a45e311b2
Fin Konnan <finkonnan@gmail.com>
2025-04-01 08:18:13 -0700

    Debugging button: Should switch from "A" to "B" and vice versa when pressed

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -1,7 +1,19 @@
 from flask_app import app
-from flask import render_template
+from flask import render_template, jsonify
+
+button_text = "A"
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", button_text=button_text)
+
+@app.route('/call-function', methods=['POST'])
+def call_function():
+    global button_text
+    if button_text == "A":
+        button_text = "B"
+    else:
+        button_text = "A"
+    print(button_text)
+    return jsonify({"button_text": button_text})
