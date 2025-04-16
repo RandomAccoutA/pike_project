c0610f32a821bcf484af8318436f0ff6bb7dae9c
Fin Konnan <finkonnan@gmail.com>
2025-04-15 18:26:08 -0700

    js/python communication works

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -1,7 +1,17 @@
 from flask_app import app
-from flask import render_template, jsonify
+from flask import render_template, jsonify, request
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template("index.html")
+
+@app.route('/button-press', methods=["POST"])
+def button_press():
+    data = request.get_json()
+
+    button_name = data.get("button_name")
+
+    print(button_name)
+
+    return jsonify({"resp": "Test Response :)"})
